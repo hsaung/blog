5c84aace0f65d6a6f838c6d329750247d8b9fcfd
hsaung <hsaung@nfu.edu.tw>
2016-01-24 15:11:06 +0800

    20160124_migration

---
+++ database/migrations/2016_01_24_063155_add_is_feature_in_post_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddIsFeatureInPostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title',500)->change();
+            $table->boolean('is_feature')->default(0)->after('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('title')->change();
+            $table->dropColumn('is_feature');
+        });
+    }
+}
